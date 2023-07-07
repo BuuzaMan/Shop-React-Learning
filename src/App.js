1d921795e7ea5ac5b599cf9f37e22e2537e6aab2
YuSo666 <julsi.08@bk.ru>
2023-07-07 19:28:40 +0800

    исправление ошибки в Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Footer from "./Components/Footer/inex";
+import Footer from "./Components/Footer/index";
 import Header from "./Components/Header";
 import React from "react";
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
     <Header />
-    <Footer />
+    <Footer/>
     </>
   )
 } 
--- src/Components/Footer/inex.js
+++ src/Components/Footer/inex.js
@@ -1,18 +1,18 @@
-import { Columns } from "../Columns"
-const service = "СЕРВИС";
-const company = "О КОМПАНИИ";
-const help = "ПОМОЩЬ";
+// import { Columns } from "../Columns"
+// const service = "СЕРВИС";
+// const company = "О КОМПАНИИ";
+// const help = "ПОМОЩЬ";
 
-export const Footer = () => {
-    return (
-        <>
-        <footer className=" text-red-500">
-        <Columns src={service}/>
-        <Columns src={company}/>
-        <Columns src={help}/>
-        </footer>
-        </>
-    )
-}
+// export const Footer = () => {
+//     return (
+//         <>
+//         <footer className=" text-red-500">
+//         <Columns src={service}/>
+//         <Columns src={company}/>
+//         <Columns src={help}/>
+//         </footer>
+//         </>
+//     )
+// }
 
-export default Footer;
+// export default Footer;
