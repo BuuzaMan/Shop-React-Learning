b68ae5be92d130524000ab5fc0e38e650369ddd9
BuuzaMan <batueveuror@gmail.com>
2023-08-26 16:42:25 +0800

    Catalog update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
-import router from './routes';
+import router from './Components/routes';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
